eb023f7269b877b1c9cfdf56b640f84c21e3cce7
Adeyemo Samuel <adeyemosamuel@gmail.com>
2018-01-06 11:41:55 +0100

    update

--- src/pages/searchmodal/searchmodal.ts
+++ src/pages/searchmodal/searchmodal.ts
@@ -8,53 +8,55 @@ import { Http } from '@angular/http';
   templateUrl: 'searchmodal.html',
 })
 export class SearchmodalPage {
-  data: Array<string> = [];
-  _data: Array<string> = [];
-  _data1: Array<string> = [];
-  data1: Array<string> = [];
-  _data2: Array<string> = [];
-  data2: Array<string> = [];
-  data3: Array<string> = [];
+  dat: Array<string> = [];
+  _dat: Array<string> = [];
+  // _data1: Array<string> = [];
+  // data1: Array<string> = [];
+  // _data2: Array<string> = [];
+  // data2: Array<string> = [];
+  // data3: Array<string> = [];
   selectedE: string = '';
 
   constructor( private http: Http,public navCtrl: NavController, public navParams: NavParams,public viewCtrl:ViewController, public modalCtrl: ModalController) {
-    let localData = this.http.get('assets/paul.json').map(res => res.json());
-    localData.subscribe(data => {
-      for (let val of data) {
-        for (let newVal of val.Product_data) {
-          this.data.push(newVal.TYPES);
-        }
-      }
-      this._data = this.data;
-      console.log(data);
-      
-    });
-    let localData1 = this.http.get('assets/sector.json').map(res => res.json());
-    localData1.subscribe(data => {
-      for (let val of data) {
-        for (let newVal of val.sector_category_data) {
-          this.data1.push(newVal.sector_type_benefits);
-        }
-      }
-      this._data1 = this.data1;
-
-      
-    });
-
-    let localData2 = this.http.get('assets/product.json').map(res => res.json());
-    localData2.subscribe(data => {
-      for (let val of data) {
+    let localData1 = this.http.get('assets/merge.json').map(res1 => res1.json());
+    localData1.subscribe(dat => {
+      for (let val of dat) {
         for (let newVal of val.category_data) {
-          this.data2.push(newVal.type_benefits);
+          this.dat.push(newVal);
         }
       }
-      this._data2 = this.data2;
+      this._dat = this.dat;
+      console.log(dat);
+      console.log(this._dat);
+      
+      
+    });
+    // let localData1 = this.http.get('assets/sector.json').map(res => res.json());
+    // localData1.subscribe(data => {
+    //   for (let val of data) {
+    //     for (let newVal of val.sector_category_data) {
+    //       this.data1.push(newVal.sector_type_benefits);
+    //     }
+    //   }
+    //   this._data1 = this.data1;
+
+      
+    // });
+
+    // let localData2 = this.http.get('assets/product.json').map(res => res.json());
+    // localData2.subscribe(data => {
+    //   for (let val of data) {
+    //     for (let newVal of val.category_data) {
+    //       this.data2.push(newVal.type_benefits);
+    //     }
+    //   }
+    //   this._data2 = this.data2;
     
       
-    });
+    // });
 
-    this.data = this.data.concat(this.data1, this.data2);
-    console.log(this.data3);
+    // this.data = this.data.concat(this.data1, this.data2);
+    // console.log(this.data3);
   }
 
   ionViewDidLoad() {
@@ -65,12 +67,12 @@ export class SearchmodalPage {
   
 
   getItems(ev) {
-    this.data = this._data;
+    this.dat = this._dat;
     var val = ev.target.value;
 
     if (val && val.trim() != '') {
-        this.data = this.data.filter((item) => {
-            return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+        this.dat = this.dat.filter((item) => {
+            return (item.keywords.toLowerCase().indexOf(val.toLowerCase()) > -1);
         });
     }
 
@@ -78,6 +80,14 @@ export class SearchmodalPage {
 
   selectCancel() {
     this.viewCtrl.dismiss('');
+  }
+
+  toggleDescription(item, title) {
+    console.log("yes");
+    // console.log(title);
+    // this.navCtrl.push('Details3Page', {
+    //   data: item, title:title
+    // });
   }
 
 }
